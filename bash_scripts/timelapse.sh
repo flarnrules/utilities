bf248d7d30383d8d7b677188d00e0cfcb99ea281
flarnrules <breifenberg11@gmail.com>
2023-11-18 02:30:56 -0600

    create timelapse 4

--- bash_scripts/timelapse.sh
+++ bash_scripts/timelapse.sh
@@ -3,13 +3,13 @@
 # ffmpeg -i /mnt/c/Users/Benjamin/Videos/2023-11-16-11-39-05.mkv -filter_complex "[0:v]setpts=PTS/12[v];[v]crop=5:550:340:65" -an /mnt/c/Users/Benjamin/Videos/timelapse_2023-11-16-11-39-05.mp4
 
 # Define Variables # Both screens are 1920 x 1080
-INPUT_PATH="/mnt/c/Users/Benjamin/Videos/Timelapses/wide_stitched_dwg3.mp4"
-OUTPUT_PATH="/mnt/c/Users/Benjamin/Videos/Timelapses/tl_dwg3_3.gif"
-CROP_WIDTH=1000
-CROP_HEIGHT=700
-CROP_X=10
-CROP_Y=25
-SPEED_FACTOR=3
+INPUT_PATH="/mnt/c/Users/Benjamin/Videos/Timelapses/drawing4.mkv"
+OUTPUT_PATH="/mnt/c/Users/Benjamin/Videos/Timelapses/tl_drawing4.mp4"
+CROP_WIDTH=603
+CROP_HEIGHT=610
+CROP_X=0
+CROP_Y=0
+SPEED_FACTOR=16
 
 # Run FFmpeg command with variables
 ffmpeg -i "$INPUT_PATH" -filter_complex "[0:v]setpts=PTS/$SPEED_FACTOR[v];[v]crop=$CROP_WIDTH:$CROP_HEIGHT:$CROP_X:$CROP_Y" -an "$OUTPUT_PATH"
